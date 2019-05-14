d8173bc4cc450ed9a102ae6ed8f5569fef3f5b30
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-05-13 22:48:43 -0700

    Wrap switch in router and update App tests

--- client/app/containers/App/index.js
+++ client/app/containers/App/index.js
@@ -8,7 +8,7 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
@@ -40,12 +40,14 @@ export default function App() {
   return (
     <div>
       <MuiThemeProvider theme={theme}>
-        <Switch>
-          <UserNav>
-            <Route exact path="/" component={HomePage} />
-            <Route component={NotFoundPage} />
-          </UserNav>
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <UserNav>
+              <Route exact path="/" component={HomePage} />
+              <Route component={NotFoundPage} />
+            </UserNav>
+          </Switch>
+        </BrowserRouter>
         <GlobalStyle />
       </MuiThemeProvider>
     </div>
