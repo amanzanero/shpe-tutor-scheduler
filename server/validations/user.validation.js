49f81999057a27672adead8f9ba653431a295be1
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-05-14 17:26:55 -0700

    make email .edu only and update .gitignore

--- server/validations/user.validation.js
+++ server/validations/user.validation.js
@@ -6,7 +6,7 @@ const Joi = require('joi')
 module.exports = {
   create: {
     body: {
-      email: Joi.string().email().required(),
+      email: Joi.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$/).required(),
       password: Joi.string().min(6).max(128).required(),
       name: Joi.string().max(128).required()
     }
