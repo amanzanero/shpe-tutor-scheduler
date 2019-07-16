8f68f4e76d45b6ce3f27b0f2375aefddb7b96ea8
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-07-15 23:14:56 -0400

    styling

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,16 +4,16 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       // light: will be calculated from palette.primary.main,
-      main: '#990000'
+      main: '#990000',
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
     secondary: {
-      main: '#FFCC00'
+      main: '#FFCC00',
       // dark: will be calculated from palette.secondary.main,
-    }
+    },
     // error: will use the default color
-  }
+  },
 });
 
 export default theme;
