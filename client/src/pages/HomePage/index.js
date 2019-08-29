5ebf2c71a16774fb1569347906caf30003b2e461
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-08-28 23:29:59 -0700

    home styling changes

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
 import UserNav from '../../components/UserNav';
 
-const HomePage = () => {
-  return <UserNav />;
+const styles = {
+  root: {
+    background: '#e8e8e8',
+    flex: 1,
+  },
+  pageBody: {
+    padding: '1em',
+  },
 };
 
-export default HomePage;
+const HomePage = props => {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <UserNav />
+      <div className={classes.root} />
+    </React.Fragment>
+  );
+};
+
+HomePage.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default withStyles(styles)(HomePage);
