e9fe4312eb552b12ef547a1521b5c3dd5eca4cc7
Andrew Manzanero <manzanero.andrew@gmail.com>
2019-09-07 12:49:34 -0700

    settings dialog component

--- client/src/components/SettingsModal/index.js
+++ client/src/components/SettingsModal/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -9,6 +8,7 @@ import { withStyles } from '@material-ui/styles';
 import { Typography } from '@material-ui/core';
 
 import { textField } from '../../theme';
+import ValidatedTextInput from '../ValidatedTextInput';
 import DropDown from '../DropDown';
 import MAJORS from '../../config/majors';
 
@@ -54,56 +54,85 @@ const DROPDOWNS = [
   },
 ];
 
-const SettingsModal = props => {
-  const { classes, isModalOpen, onToggleModal } = props;
+class SettingsModal extends React.Component {
+  componentWillMount() {
+    FIELDS.forEach(textContent => {
+      this.setState(previousState => {
+        return {
+          ...previousState,
+          [textContent.stateSlug]: '',
+        };
+      });
+    });
+    DROPDOWNS.forEach(dropDown => {
+      this.setState(previousState => {
+        return {
+          ...previousState,
+          [dropDown.name]: '',
+        };
+      });
+    });
+  }
 
-  return (
-    <div>
-      <Dialog
-        open={isModalOpen}
-        onClose={onToggleModal}
-        aria-labelledby="form-dialog-title"
-      >
-        <div className={classes.titleContainer}>
-          <Typography variant="h4" className={classes.title}>
-            Login
-          </Typography>
-        </div>
-        <DialogContent>
-          {/* {FIELDS.map(cont => (
-            <TextField
-              autoFocus
-              className={classes.formfield}
-              key={cont.label}
-              margin="normal"
-              label={cont.label}
-              type={cont.type}
-              fullWidth
-              id="outlined-dense"
-              variant="outlined"
-              value={stateMap[cont.type]}
-              onChange={e => setStateMap[cont.type](e.target.value)}
-            />
-          ))}
-          {DROPDOWNS.map(item => {
-            const { [item.name]: val } = this.state;
-            const dropDownProps = {
-              name: item.name,
-              options: item.options,
-              handleDropDown: this.handleDropDown,
-              label: item.label,
-              dropDown: val,
-            };
-            return <DropDown key={item.name} {...dropDownProps} />;
-          })} */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => console.log('hi')}>Update</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-};
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleDropDown = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { classes, isModalOpen, onToggleModal } = this.props;
+    return (
+      <div>
+        <Dialog
+          open={isModalOpen}
+          onClose={onToggleModal}
+          aria-labelledby="form-dialog-title"
+        >
+          <div className={classes.titleContainer}>
+            <Typography variant="h4" className={classes.title}>
+              User Profile
+            </Typography>
+          </div>
+          <DialogContent>
+            {FIELDS.map(textContent => {
+              const { [textContent.stateSlug]: val } = this.state;
+              const inputProps = {
+                textContent,
+                val,
+                handleChange: this.handleChange,
+              };
+              return (
+                <ValidatedTextInput
+                  key={textContent.stateSlug}
+                  {...inputProps}
+                />
+              );
+            })}
+            {DROPDOWNS.map(item => {
+              const { [item.name]: val } = this.state;
+              const dropDownProps = {
+                name: item.name,
+                options: item.options,
+                handleDropDown: this.handleDropDown,
+                label: item.label,
+                dropDown: val,
+              };
+              return <DropDown key={item.name} {...dropDownProps} />;
+            })}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => console.log('hi')}>Update</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
 
 SettingsModal.propTypes = {
   classes: PropTypes.objectOf(PropTypes.any).isRequired,
