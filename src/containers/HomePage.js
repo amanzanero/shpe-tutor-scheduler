7628a58ab6a7a7f3869ec8b36742533947582c9b
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-01-06 11:44:51 -0800

    simplify homepage container

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { toggleAddCoursesModal } from '../actions';
 
 import HomePageComp from '../pages/HomePage';
 import User from './User';
 
-function HomePage(props) {
-  const { isLoading, user, onToggleAddCourses } = props;
+function HomePage() {
+  const dispatch = useDispatch();
+  const { isLoading, user } = useSelector(({ homePage, globalStore }) => ({
+    isLoading: homePage.loading,
+    user: globalStore.user,
+  }));
+  const onToggleAddCourses = useCallback(() => {
+    dispatch(toggleAddCoursesModal());
+  }, [dispatch]);
 
   const homePageProps = { isLoading, user, onToggleAddCourses };
 
@@ -18,20 +25,4 @@ function HomePage(props) {
   );
 }
 
-const mapStateToProps = ({ globalStore, homePage }) => {
-  return {
-    isLoading: homePage.loading,
-    user: globalStore.user,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onToggleAddCourses: () => dispatch(toggleAddCoursesModal()),
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(HomePage);
+export default HomePage;
