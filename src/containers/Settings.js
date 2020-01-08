3646ad8c3b1afe1b3210a5a4ec89dadf5d5f8774
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-01-08 12:37:09 -0800

    use hooks in settings

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import SettingsModal from '../components/SettingsModal';
 import { toggleSettingsModal } from '../actions';
 
 function Settings(props) {
-  const { onToggleModal, isModalOpen, user } = props;
+  const dispatch = useDispatch();
+  const onToggleModal = useCallback(() => dispatch(toggleSettingsModal()), [
+    dispatch,
+  ]);
+
+  const { isModalOpen, user } = useSelector(({ globalStore, homePage }) => ({
+    isModalOpen: homePage.settingsOpen,
+    user: globalStore.user,
+  }));
+
   const modalProps = { onToggleModal, isModalOpen, user };
 
   return <SettingsModal {...modalProps} />;
 }
 
-const mapStateToProps = ({ globalStore, homePage }) => {
-  return {
-    isModalOpen: homePage.settingsOpen,
-    user: globalStore.user,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onToggleModal: () => dispatch(toggleSettingsModal()),
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Settings);
+export default Settings;
